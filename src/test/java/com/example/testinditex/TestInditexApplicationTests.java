9acfd67d751ee7e2b0e120cd80268271af95b301
NachoValdes <nachovaldes23@gmail.com>
2024-04-11 12:41:28 +0200

    TestInditex-InitialCommits

---
+++ src/test/java/com/example/testinditex/TestInditexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testinditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestInditexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
